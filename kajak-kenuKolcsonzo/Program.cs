f53dfc74fa366828ff7af4c271c71848226a5ded
zsiroskata <78079466+zsiroskata@users.noreply.github.com>
2025-01-27 10:26:30 +0100

    6 feladat

--- kajak-kenuKolcsonzo/Program.cs
+++ kajak-kenuKolcsonzo/Program.cs
@@ -15,7 +15,7 @@ using StreamReader sr = new StreamReader(
 Console.WriteLine("4.feladat");
 Console.WriteLine($"{kolcsonzesek.Count} kölcsönzés adatai találhatók a forrásállományban");
 
-
+Console.WriteLine("\n5.feladat");
 Console.Write("Add meg a neved: ");
 string nev = Console.ReadLine().ToLower();
 
@@ -27,10 +27,14 @@ if (talalatok.Count == 0)
 }
 else
 {
-    Console.WriteLine($"\n{nev.ToUpper()} kölcsönzései:");
+    Console.WriteLine($"{nev.ToUpper()} kölcsönzései:");
     foreach (var kolcsonzes in talalatok)
     {
         Console.WriteLine($"  {kolcsonzes.ElvitelOraja}:{kolcsonzes.ElvitelPerce:00} - {kolcsonzes.VisszahozatalOraja}:{kolcsonzes.VisszahozatalPerc:00}");
     }
 }
 
+Console.WriteLine("\n6.feladat");
+var n = kolcsonzesek.FindAll(k => k.Nev.Contains(","));
+
+Console.WriteLine($"Külföldiek száma: {n.Count}\nMagyarok száma: {kolcsonzesek.Count- n.Count}");
